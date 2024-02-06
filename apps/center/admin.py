149a4c87c285f68bf21663d9d936989a5e26e741
otlevire <eriveltoclenio@gmail.com>
2024-02-06 20:35:04 +0000

    feat: models database Changes to be committed: 	modified:   apps/center/admin.py 	modified:   apps/center/migrations/0001_initial.py 	deleted:    apps/center/migrations/0002_course_created_at_course_updated_at.py 	modified:   apps/center/models.py 	modified:   db.sqlite3

--- apps/center/admin.py
+++ apps/center/admin.py
@@ -1,22 +1,67 @@
 from django.contrib import admin
-from .models import Student, Course
+from .models import *
 
-
-# Register your models here.
 class ListStudents(admin.ModelAdmin):
-    list_display = ['id', 'name', 'bi','created_at']
-    list_display_links = ['id', 'name', 'bi']
-    search_fields = ['name','bi']
-    list_filter = ['id', "name"]
+    list_display = ['user','bi', 'gender','race','civil_status','birthday','created_at',]
+    list_display_links = ['user','bi', 'birthday', 'created_at',]
+    search_fields = ['user','bi','birthday',]
+    list_filter = ['id', 'birthday','gender','race','civil_status',]
     list_per_page = 20
+admin.site.register(Student, ListStudents)
+
+class ListTeachers(admin.ModelAdmin):
+    list_display = ['user','bi', 'formation_area','gender','race','civil_status','created_at',]
+    list_display_links = ['user','bi','formation_area','created_at',]
+    search_fields = ['user','bi', 'formation_area']
+    list_filter = ['id', 'gender','race','civil_status',]
+    list_per_page = 20
+
+admin.site.register(Teacher, ListTeachers)
+
 
 class ListCourses(admin.ModelAdmin):
-    list_display = ['id', 'code', 'description','level','created_at']
-    list_display_links =  ['id', 'code', 'description','level']
-    search_fields = ['code','description']
-    list_filter = ['id', 'code','description']
+    list_display = ['id', 'code', 'description','duraction','duraction_unit','created_at']
+    list_display_links =  ['id', 'code', 'description','duraction', 'duraction_unit']
+    search_fields = ['code','description','duraction']
+    list_filter = ['id', 'code','description', 'duraction']
     list_per_page = 20
 
+admin.site.register(Course,ListCourses)
 
-admin.site.register(Student, ListStudents)
-admin.site.register(Course, ListCourses)
+class ListRooms(admin.ModelAdmin):
+    list_display = ['id', 'name', 'number',]
+    list_display_links =  ['id', 'name', 'number',]
+    search_fields = ['name','number',]
+    list_filter = ['id', 'name','number',]
+    list_per_page = 20
+
+admin.site.register(Room, ListRooms)
+
+
+class ListClassRoom(admin.ModelAdmin):
+    list_display = ['id', 'course', 'teacher','room','status']
+    list_display_links =  ['id', 'course', 'teacher','room', 'status']
+    search_fields = ['course','teacher','room', 'status']
+    list_filter = ['id', 'course','teacher', 'room', 'status',]
+    list_per_page = 20
+
+admin.site.register(ClassRoom,ListClassRoom)
+
+class ListRegistration(admin.ModelAdmin):
+    list_display = ['id', 'class_room','student','data']
+    list_display_links =  ['id', 'class_room','student','data']
+    search_fields = ['class_room','student','data']
+    list_filter = ['id', 'class_room','student','data',]
+    list_per_page = 20
+
+admin.site.register(Registration,ListRegistration)
+
+class ListNote(admin.ModelAdmin):
+    list_display = ['id', 'teacher','student','value','class_room',]
+    list_display_links =  ['id', 'teacher','student','value','class_room',]
+    search_fields = ['teacher','student','value','class_room',]
+    list_filter = ['id', 'teacher','student','value','class_room',]
+    list_per_page = 20
+
+admin.site.register(Note,ListNote)
+
--- apps/center/models.py
+++ apps/center/models.py
@@ -1,29 +1,104 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here
+
+GENDER=(('M','MALE'), ('F', 'FEMALE'))
+RACE=(('B','BLACK'), ('W', 'WHITE'))
+CIVIL_STATUS=(('S','SINGLE'), ('M', 'MARRIED'),('D','DIVORCED'),('W','WIDOWER'))
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    bi = models.CharField(max_length=14)
-    birthday = models.DateField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bi = models.CharField(max_length=14, null=False, blank=False)
+    birthday = models.DateField(null=False, blank=False)
+    gender = models.CharField(max_length=1, choices=GENDER, default='M')
+    race = models.CharField(max_length=1, choices=RACE, default='B')
+    civil_status = models.CharField(max_length=1, choices=CIVIL_STATUS, default='S')
+    natianality = models.CharField(max_length=100, null=False, blank=False)     
     created_at = models.DateTimeField(auto_now=True)    
     updated_at = models.DateTimeField(auto_now_add=True)    
 
-    def __str__(self) -> str:
-        return f"{self.name}"
+    def get_full_name(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name}"
     
+    def __str__(self) -> str:
+        return self.get_full_name()
+    
+    
+class Teacher(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bi = models.CharField(max_length=14, null=False, blank=False)
+    birthday = models.DateField(null=False, blank=False)
+    gender = models.CharField(max_length=1, choices=GENDER, default='M')
+    race = models.CharField(max_length=1, choices=RACE, default='B')
+    civil_status = models.CharField(max_length=1, choices=CIVIL_STATUS, default='S')
+    natianality = models.CharField(max_length=100, null=False, blank=False)
+    formation_area = models.CharField(max_length=100, null=False, blank=False)  
+    formation_title = models.CharField(max_length=100, null=False, blank=False)   
+    created_at = models.DateTimeField(auto_now=True)    
+    updated_at = models.DateTimeField(auto_now_add=True)    
+
+    def get_full_name(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+    def __str__(self) -> str:
+        return self.get_full_name()
+    
+
 class Course(models.Model):
-    LEVELS = (('SS', 'LEGEND'), 
-              ('S',  'START'),
-              ('A',  'ADVANCED'),
-              ('B',  'ALMOST-ADVANCED'),
-              ('C',  'INTERMEDIATE'),
-              ('D',  'PRE-INTERMEDIATE'),
-              ('E',  'BASIC'))
-    code = models.CharField(max_length=10)
-    description = models.CharField(max_length=255)
-    level = models.CharField(max_length=10,choices=LEVELS, default='E',blank=False, null=False)
+    code = models.CharField(max_length=10, null=False, blank=False)
+    description = models.CharField(max_length=255,null=False, blank=False)
+    duraction = models.IntegerField(null=False, blank=False)
+    duraction_unit = models.CharField(max_length=10,null=False, blank=False)
     created_at = models.DateTimeField(auto_now=True)    
     updated_at = models.DateTimeField(auto_now_add=True) 
     
     def __str__(self) -> str:
         return f"{self.description}"
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    number = models.IntegerField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True)    
+    updated_at = models.DateTimeField(auto_now_add=True) 
+    
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
+
+class ClassRoom(models.Model):
+    STATUS = (('MARKED','MARKED'),('STARTED','STARTED'),('FINISHED','FINISHED'))
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
+    status = models.CharField(max_length=50, choices=STATUS)
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+    created_at = models.DateTimeField(auto_now=True)    
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.course.code} - {self.course.description}"
+    
+    def period(self) -> str:
+        return f"---PERIOD NOT SET---"
+
+
+class Registration(models.Model):
+    class_room = models.ForeignKey(ClassRoom, on_delete=models.SET_NULL, null=True)
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    data= models.DateField(auto_now=True) 
+    updated_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self) -> str:
+        return f"{self.student}- {self.class_room}"
+    
+
+class Note(models.Model):
+    teacher = models.ForeignKey(Teacher,on_delete=models.SET_NULL, null=True)
+    class_room = models.ForeignKey(ClassRoom,on_delete=models.SET_NULL, null=True)
+    student = models.ForeignKey(Student,on_delete=models.CASCADE, null=False)
+    value =  models.IntegerField(default=0)
+    unit = models.CharField(max_length=10, default="")
+
+    created_at = models.DateTimeField(auto_now=True)    
+    updated_at = models.DateTimeField(auto_now_add=True)
